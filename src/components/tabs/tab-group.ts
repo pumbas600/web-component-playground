9ca4c40751d7f86544094f5012d9e63d6e23236e
pumbas600 <joshjeffers600@gmail.com>
2026-05-07 21:26:15 +1200

    tab group: add selected property

--- src/components/tabs/tab-group.ts
+++ src/components/tabs/tab-group.ts
@@ -6,6 +6,10 @@ import WebComponent from "../web-component";
 export default class TabGroup extends WebComponent {
   public static readonly TagName = "pb-tab-group";
 
+  public static get observedAttributes(): string[] {
+    return ["selected"];
+  }
+
   constructor() {
     super(template);
     this.onSlotChange = this.onSlotChange.bind(this);
@@ -16,18 +20,55 @@ export default class TabGroup extends WebComponent {
     }
   }
 
+  public get selected(): string {
+    return this.getAttribute("selected") ?? "";
+  }
+
+  public set selected(selectedTabName: string) {
+    this.setAttribute("selected", selectedTabName);
+  }
+
   public allTabs(): Tab[] {
     return Array.from(this.querySelectorAll<Tab>(`:scope > ${Tab.TagName}`));
   }
 
   public allTabPanels(): TabPanel[] {
-    return Array.from(
-      this.querySelectorAll<TabPanel>(`:scope > ${TabPanel.TagName}`),
-    );
+    return Array.from(this.querySelectorAll<TabPanel>(`:scope > ${TabPanel.TagName}`));
+  }
+
+  public findTabWithName(name: string): Tab | null {
+    return this.allTabs().find((tab) => tab.name === name) ?? null;
   }
 
   protected connectedCallback() {
     console.debug("Connected");
+  }
+
+  protected attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null): void {
+    console.debug(name, oldValue, newValue);
+
+    switch (name) {
+      case "selected":
+        if (oldValue !== null) {
+          const currentSelectedTab = this.findTabWithName(oldValue);
+          if (currentSelectedTab) {
+            this.setSelectedTabAttributes(currentSelectedTab, false);
+          }
+        }
+
+        if (newValue !== null) {
+          const selectedTab = this.findTabWithName(newValue);
+          if (selectedTab) {
+            this.setSelectedTabAttributes(selectedTab, true);
+          }
+        }
+        break;
+    }
+  }
+
+  private setSelectedTabAttributes(tab: Tab, isSelected: boolean): void {
+    tab.setAttribute("aria-selected", String(isSelected));
+    tab.tabIndex = isSelected ? 0 : -1;
   }
 
   private onSlotChange() {
